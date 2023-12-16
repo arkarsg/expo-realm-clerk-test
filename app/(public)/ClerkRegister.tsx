d76f377941541181776f058a7bf4d90ab180b160
arkarsg <alxarkar@gmail.com>
2023-12-16 20:46:08 +0800

    Complete Atlas Clerk integration

--- app/(public)/ClerkRegister.tsx
+++ app/(public)/ClerkRegister.tsx
@@ -1,124 +1,156 @@
-import { Button, TextInput, View, StyleSheet } from 'react-native';
-import { useSignUp } from '@clerk/clerk-expo';
-import Spinner from 'react-native-loading-spinner-overlay';
-import { useState } from 'react';
-import { Stack } from 'expo-router';
+import { Button, TextInput, View, StyleSheet } from "react-native";
+import { useSignUp, useClerk } from "@clerk/clerk-expo";
+import Spinner from "react-native-loading-spinner-overlay";
+import { useState } from "react";
+import { Stack } from "expo-router";
+import { useAuth as useRealmAuth } from "@realm/react";
 
 const register = () => {
-	const { isLoaded, signUp, setActive } = useSignUp();
+  const { isLoaded, signUp, setActive } = useSignUp();
 
-	const [emailAddress, setEmailAddress] = useState('');
-	const [password, setPassword] = useState('');
-	const [pendingVerification, setPendingVerification] = useState(false);
-	const [code, setCode] = useState('');
-	const [loading, setLoading] = useState(false);
+  const clerk = useClerk();
+  const { logInWithJWT, result } = useRealmAuth();
 
-	const onSignUpPress = async () => {
-		if (!isLoaded) {
-			return;
-		}
-		setLoading(true);
+  const [username, setUsername] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      // Create the user on Clerk
+      await signUp.create({
+        username,
+        emailAddress,
+        password,
+      });
+
+      // Send verification Email
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to verify the email address
+      setPendingVerification(true);
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      await setActive({ session: completeSignUp.createdSessionId });
+      const token = await clerk.session?.getToken({ template: 'Atlas'});
+      if (token) {
+		console.log(token);
 		try {
-			// Create the user on Clerk
-			await signUp.create({
-				emailAddress,
-				password
-			});
-
-			// Send verification Email
-			await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
-
-			// change the UI to verify the email address
-			setPendingVerification(true);
-		} catch (err: any) {
-			alert(err.errors[0].message);
-		} finally {
-			setLoading(false);
+			logInWithJWT(token);
+		} catch (err) {
+			console.log("🛑 Error logging into Realm", err)
 		}
-	};
+      } else {
+        console.log("🛑 Could not get token from Clerk");
+      }
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	const onPressVerify = async () => {
-		if (!isLoaded) {
-			return;
-		}
-		setLoading(true);
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
+      <Spinner visible={loading} />
 
-		try {
-			const completeSignUp = await signUp.attemptEmailAddressVerification({
-				code
-			});
+      {!pendingVerification && (
+        <>
+          <TextInput
+            autoCapitalize="none"
+            placeholder="username"
+            value={username}
+            onChangeText={setUsername}
+            style={styles.inputField}
+          />
+          <TextInput
+            autoCapitalize="none"
+            placeholder="simon@galaxies.dev"
+            value={emailAddress}
+            onChangeText={setEmailAddress}
+            style={styles.inputField}
+          />
+          <TextInput
+            placeholder="password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={styles.inputField}
+          />
 
-			await setActive({ session: completeSignUp.createdSessionId });
-		} catch (err: any) {
-			alert(err.errors[0].message);
-		} finally {
-			setLoading(false);
-		}
-	};
+          <Button
+            onPress={onSignUpPress}
+            title="Sign up"
+            color={"#6c47ff"}
+          ></Button>
+        </>
+      )}
 
-	return (
-		<View style={styles.container}>
-			<Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
-			<Spinner visible={loading} />
-
-			{!pendingVerification && (
-				<>
-					<TextInput
-						autoCapitalize="none"
-						placeholder="simon@galaxies.dev"
-						value={emailAddress}
-						onChangeText={setEmailAddress}
-						style={styles.inputField}
-					/>
-					<TextInput
-						placeholder="password"
-						value={password}
-						onChangeText={setPassword}
-						secureTextEntry
-						style={styles.inputField}
-					/>
-
-					<Button onPress={onSignUpPress} title="Sign up" color={'#6c47ff'}></Button>
-				</>
-			)}
-
-			{pendingVerification && (
-				<>
-					<View>
-						<TextInput
-							value={code}
-							placeholder="Code..."
-							style={styles.inputField}
-							onChangeText={setCode}
-						/>
-					</View>
-					<Button onPress={onPressVerify} title="Verify Email" color={'#6c47ff'}></Button>
-				</>
-			)}
-		</View>
-	);
+      {pendingVerification && (
+        <>
+          <View>
+            <TextInput
+              value={code}
+              placeholder="Code..."
+              style={styles.inputField}
+              onChangeText={setCode}
+            />
+          </View>
+          <Button
+            onPress={onPressVerify}
+            title="Verify Email"
+            color={"#6c47ff"}
+          ></Button>
+        </>
+      )}
+    </View>
+  );
 };
 
 export default register;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		padding: 20
-	},
-	inputField: {
-		marginVertical: 4,
-		height: 50,
-		borderWidth: 1,
-		borderColor: '#6c47ff',
-		borderRadius: 4,
-		padding: 10,
-		backgroundColor: '#fff'
-	},
-	button: {
-		margin: 8,
-		alignItems: 'center'
-	}
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#6c47ff",
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    margin: 8,
+    alignItems: "center",
+  },
 });
